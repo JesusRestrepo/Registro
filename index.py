06c36b11f889f02d6da38eee54062337df41418e
JesusRestrepo <jesusrestrepo2003@gmail.com>
2021-05-11 15:54:00 -0500

    V2.2

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ from flask_mysqldb import MySQL
 app=Flask(__name__)
 
 #conexion base de datos
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '45.236.129.6'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'Jesus-Restrepo.2003'
 app.config['MYSQL_DB'] = 'registro'
